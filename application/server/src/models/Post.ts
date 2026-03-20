bc98067934f50bad8ce6f89acaabb95dbc028759
junseinagao <junsei.nagao@gmail.com>
2026-03-20 18:13:25 +0900

    Post.id has a UUID default value in Sequelize, so it should not be   required in creation attributes.

--- application/server/src/models/Post.ts
+++ application/server/src/models/Post.ts
@@ -14,7 +14,7 @@ import { Sound } from "@web-speed-hackathon-2026/server/src/models/Sound";
 import { User } from "@web-speed-hackathon-2026/server/src/models/User";
 
 export class Post extends Model<InferAttributes<Post>, InferCreationAttributes<Post>> {
-  declare id: string;
+  declare id: CreationOptional<string>;
   declare userId: ForeignKey<User["id"]>;
   declare movieId?: ForeignKey<Movie["id"]>;
   declare soundId?: ForeignKey<Sound["id"]>;
